e15e6a7efb057f80ed07684f740c13048f06323e
RRokas <rokas.ringaila@gmail.com>
2023-08-22 19:46:06 +0300

    Improved tests and code coverage.

--- tests/Core.Tests/BomComparisonServiceTests.cs
+++ tests/Core.Tests/BomComparisonServiceTests.cs
@@ -2,7 +2,7 @@ using Core.Entitites;
 
 namespace Core.Tests;
 
-public class BomLineComparisonTests
+public class BomComparisonServiceTests
 {
     [Fact]
     public void AddedDesignator()
@@ -36,6 +36,7 @@ public class BomLineComparisonTests
         var result = comparer.CompareBomLine(source, target);
         Assert.Equal(4, result.Designators.Count);
         Assert.Equal(DesignatorComparisonStatus.Added, result.Designators[3].DesignatorComparisonStatus);
+        Assert.Equal(BomLineComparisonStatus.Modified, result.ComparisonStatus);
     }
     
     [Fact]
@@ -104,8 +105,80 @@ public class BomLineComparisonTests
             x => Assert.Equal(DesignatorComparisonStatus.Unchanged, x.DesignatorComparisonStatus));
     }
     
+
     [Fact]
-    public void MixedResults()
+    public void NewBomLineInTargetHasComparisonStatusOfAdded()
+    {
+        var comparer = new BomComparisonService();
+        
+        var sourceBom = new List<BomLine>();
+        
+        var targetBom = new List<BomLine>
+        {
+            new BomLine
+            {
+                Quantity = 1,
+                PartNumber = "321",
+                Designators = new List<Designator>
+                {
+                    new Designator { Name = "R1" },
+                    new Designator { Name = "R3" },
+                    new Designator { Name = "R4" },
+                }
+            }
+        };
+        
+        var result = comparer.CompareBom(sourceBom, targetBom);
+        Assert.Single(result);
+        Assert.Equal(BomLineComparisonStatus.Added, result[0].ComparisonStatus);
+    }
+
+    [Fact]
+    public void RemovedBomLineInTargetHasComparisonStatusOfRemoved()
+    {
+        var comparer = new BomComparisonService();
+        
+        var sourceBom = new List<BomLine>
+        {
+            new BomLine
+            {
+                Quantity = 1,
+                PartNumber = "321",
+                Designators = new List<Designator>
+                {
+                    new Designator { Name = "R1" },
+                    new Designator { Name = "R3" },
+                    new Designator { Name = "R4" },
+                }
+            }
+        };
+
+        var targetBom = new List<BomLine>();
+        
+        var result = comparer.CompareBom(sourceBom, targetBom);
+        Assert.Single(result);
+        Assert.Equal(BomLineComparisonStatus.Removed, result[0].ComparisonStatus);
+    }
+
+    [Fact]
+    public void ComparingDifferentPartsThrowsException()
+    {
+        var comparer = new BomComparisonService();
+        var source = new BomLine
+        {
+            PartNumber = "123",
+        };
+        
+        var target = new BomLine
+        {
+            PartNumber = "321"
+        };
+        
+        Assert.Throws<ArgumentException>(() => comparer.CompareBomLine(source, target));
+    }
+    
+    [Fact]
+    public void CompareToNullThrowsArgumentNull()
     {
         var comparer = new BomComparisonService();
         var source = new BomLine
@@ -119,24 +192,9 @@ public class BomLineComparisonTests
                 new Designator { Name = "R3" },
             }
         };
-        
-        var target = new BomLine
-        {
-            Quantity = 1,
-            PartNumber = "123",
-            Designators = new List<Designator>
-            {
-                new Designator { Name = "R1" },
-                new Designator { Name = "R3" },
-                new Designator { Name = "R4" },
-            }
-        };
-        
-        var result = comparer.CompareBomLine(source, target);
-        Assert.Equal(4, result.Designators.Count);
-        Assert.Equal(DesignatorComparisonStatus.Unchanged, result.Designators[0].DesignatorComparisonStatus);
-        Assert.Equal(DesignatorComparisonStatus.Removed, result.Designators[1].DesignatorComparisonStatus);
-        Assert.Equal(DesignatorComparisonStatus.Unchanged, result.Designators[2].DesignatorComparisonStatus);
-        Assert.Equal(DesignatorComparisonStatus.Added, result.Designators[3].DesignatorComparisonStatus);
+
+        Assert.Throws<ArgumentNullException>(() => comparer.CompareBomLine(source, null));
+        Assert.Throws<ArgumentNullException>(() => comparer.CompareBomLine(null, source));
+        Assert.Throws<ArgumentNullException>(() => comparer.CompareBomLine(null, null));
     }
 }
