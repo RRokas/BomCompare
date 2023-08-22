abb9e556546a02273234faa98626087823917f88
RRokas <rokas.ringaila@gmail.com>
2023-08-23 00:50:24 +0300

    xls and xlsx is supported (tested), changed to a more generic term.

--- src/CLI/Program.cs
+++ src/CLI/Program.cs
@@ -6,7 +6,7 @@ var app = new CommandApp();
 app.Configure(config =>
 {
     config.AddCommand<CompareBom>("compare")
-        .WithDescription("Compares two BOM xlsx files and outputs the comparison to a new xlsx file.")
+        .WithDescription("Compares two BOM Excel files and outputs the comparison to a new xlsx file.")
         .WithExample("compare -s source.xlsx -t target.xlsx -o output.xlsx");
 });
 
