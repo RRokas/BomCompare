4605b518e754a5466818df6ff1ebd86f705cd547
RRokas <rokas.ringaila@gmail.com>
2023-08-28 00:44:50 +0300

    unnecessary property

--- src/Core/ExcelHandling/Npoi/NpoiRowToBomMapping.cs
+++ src/Core/ExcelHandling/Npoi/NpoiRowToBomMapping.cs
@@ -6,6 +6,5 @@ namespace Core.ExcelHandling.Npoi;
 public class NpoiRowToBomMapping
 {
     public PropertyInfo PropertyInfo { get; set; }
-    public ExcelColumnNameAttribute ExcelColumnNameAttribute { get; set; }
     public int IndexInExcel { get; set; }
 }
